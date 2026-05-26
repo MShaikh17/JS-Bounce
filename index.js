5e3925542fda7f1c023144b23f9a29b9e42cff0a
MShaikh17 <muaaz.shaikh@ukaea.uk>
2026-05-26 16:36:55 +0100

    added a function to change the ball colour when it hits the wall. Fixed some variable naming issues.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ console.log("working");
 
 const canvas = document.getElementById("canvas");
 const view = canvas.getContext("2d");
+const radius = 40;
+
+let ballcolour = getRandomColor();
 
 function resize() {
   canvas.width = window.innerWidth;
@@ -19,18 +22,38 @@ let dy = 5;
 let x = 100;
 let y = 100;
 
+function getRandomColor() {
+  return `rgb(
+  {Math.floor(Math.random() * 256)},
+  {Math.floor(Math.random() * 256)},
+  {Math.floor(Math.random() * 256)},
+  )';`;
+}
+
 function animate() {
   view.clearRect(0, 0, canvas.width, canvas.height);
   x += dx;
   y += dy;
-  if (y + 40 > canvas.height || y < 40) {
+
+  let WallHit = false;
+
+  if (y + radius > canvas.height || y - radius < 40) {
     dy = -dy;
+    WallHit = true;
   }
-  if (x + 40 > canvas.width || x < 40) {
+  if (x + radius > canvas.width || x - radius < 40) {
     dx = -dx;
+    WallHit = true;
   }
+
+  if (WallHit) {
+    ballcolor = getRandomColor();
+  }
+
   view.beginPath();
-  view.arc(x, y, 40, 0, Math.PI * 2);
+  view.arc(x, y, radius, 0, Math.PI * 2);
+  view.fillStyle = ballcolor();
+  view.arc(x, y, radius, 0, Math.PI * 2);
   view.fill();
   requestAnimationFrame(animate);
 }
