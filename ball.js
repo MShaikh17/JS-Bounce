190f56f90c8a629cd29e9b10355272bbe32c0f83
Muaaz <muaaz.rshaikh@gmail.com>
2026-06-17 08:15:36 +0100

    A new file ball.js is added, which is the class for ball objects. This turns the code into an object-oriented program. The index.js file is modified to create multiple ball objects and animate them on the canvas. Includes changes to speed and radius size. The index.html file is modified to include the ball.js script.

---
+++ ball.js
@@ -0,0 +1,44 @@
+const canvas = document.getElementById("canvas");
+const view = canvas.getContext("2d");
+
+export class Ball {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    const maxSpeed = 10;
+    this.dx = Math.random() * maxSpeed - maxSpeed / 2;
+    this.dy = Math.random() * maxSpeed - maxSpeed / 2;
+    this.radius = 3;
+    this.color = this.getRandomColor();
+  }
+  update() {
+    this.x += this.dx;
+    this.y += this.dy;
+
+    let WallHit = false;
+
+    if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+      WallHit = true;
+    }
+
+    if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+      WallHit = true;
+    }
+  }
+
+  updateDraw() {
+    view.beginPath();
+    view.fillStyle = this.color;
+    view.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    view.fill();
+  }
+
+  getRandomColor() {
+    return `rgb(
+    ${Math.floor(Math.random() * 256)},
+    ${Math.floor(Math.random() * 256)},
+    ${Math.floor(Math.random() * 256)} )`;
+  }
+}
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { Ball } from "./ball.js";
+
 console.log("working");
 
 const canvas = document.getElementById("canvas");
@@ -5,8 +7,6 @@ const view = canvas.getContext("2d");
 
 // this is the radius of the ball.
 const radius = 40;
-
-let ballcolor = getRandomColor();
 
 // This resizes the canvas to fit the window and set stroke and fill styles for the ball.
 function resize() {
@@ -22,46 +22,28 @@ resize();
 //this is the event listener for resizing the window
 //It calls the resize function to adjust the canvas size and styles accordingly.
 window.addEventListener("resize", resize);
-let dx = 5;
-let dy = 5;
-let x = 100;
-let y = 100;
-
-//this function generates a random rgb colour string for the ball when it hits a wall.
-function getRandomColor() {
-  return `rgb(
-  ${Math.floor(Math.random() * 256)},
-  ${Math.floor(Math.random() * 256)},
-  ${Math.floor(Math.random() * 256)} )`;
+const balls = [];
+const numballs = 1000;
+for (let i = 0; i < numballs; i++) {
+  //this function generates a random rgb colour string for the ball when it hits a wall.
+  const x = Math.floor(Math.random() * canvas.width);
+  const y = Math.floor(Math.random() * canvas.height);
+  const ball = new Ball(x, y);
+  balls.push(ball);
 }
-
 // this animates the ball to move around the canvas and bounce off walls.
 function animate() {
   view.clearRect(0, 0, canvas.width, canvas.height);
-  x += dx;
-  y += dy;
 
-  let WallHit = false;
-
-  if (y + radius > canvas.height || y - radius < 0) {
-    dy = -dy;
-    WallHit = true;
-  }
-
-  if (x + radius > canvas.width || x - radius < 0) {
-    dx = -dx;
-    WallHit = true;
-  }
-
-  if (WallHit) {
+  /* if (WallHit) {
     ballcolor = getRandomColor();
-  }
+  }*/
 
   //this draws the ball on the canvas at its new position with the current colour.
-  view.beginPath();
-  view.fillStyle = ballcolor;
-  view.arc(x, y, radius, 0, Math.PI * 2);
-  view.fill();
+  for (const ball of balls) {
+    ball.update();
+    ball.updateDraw();
+  }
   requestAnimationFrame(animate);
 }
 
